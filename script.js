3502efbd96242345a42c3603feb23ea3bd203144
Kyle <kyleferguson91@gmail.com>
2022-08-25 08:17:43 -0400

    added css

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 
-
+const screen = document.querySelector(".screentexts")
 function add (a,b) {
-    console.log(a+b)
-    return a + b
+    console.log(+a + +b)
+   // screen.textContent = (+a + +b)
+    return +a + +b
 }
 
 function subtract (a,b) {
     console.log(a-b)
-    return a-b
+  //  screen.textContent = +a - +b
+    return +a - +b
 }
 
 function multiply (a,b) {
-
-    return a*b
+   // screen.textContent = +a * +b
+    return +a * +b
 }
+  
+
 
 function divide (a,b) {
-
-    return a/b
+   // screen.textContent = +a / +b
+    return +a / +b
 }
 
 function operate(operator,a,b) {
@@ -33,4 +37,77 @@ else if (operator == "/") {divide(a,b)}
 }
 
 
-(operate("-",2,3))
+
+
+
+
+
+
+
+const buttons = document.querySelectorAll("button")
+
+
+let input = []
+
+
+buttons.forEach((elem,ind,arr) => {
+
+
+
+    elem.addEventListener("click", (e) => {
+        
+
+      
+
+        let displayValue = e.target.textContent
+
+        if (e.target.textContent == "clear") {
+            screen.textContent = ""
+            input = []
+            console.log(input)}
+
+
+        else if (e.target.textContent == "<") {
+           console.log( )
+            
+             input.pop()
+             screen.textContent = input.join("")
+                console.log(input)}
+
+                else if (e.target.textContent == "+" ||
+                         e.target.textContent == "-" ||
+                         e.target.textContent == "*" ||
+                         e.target.textContent == "/") {
+                           
+                            input.push(e.target.textContent)
+                            screen.textContent = "" 
+                            console.log(input)   
+
+
+
+                }
+          
+
+
+        else if (e.target.textContent == "=") {
+            screen.textContent += e.target.textContent
+           operate(input[1], input[0], input[2]) 
+      
+            
+        }
+
+        else {
+            
+            input.push(e.target.textContent)
+        console.log(input)}
+        
+
+
+
+
+    })
+        
+
+
+
+})
