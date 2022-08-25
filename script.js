7402f01365f16b2613ed7f80171b46aaff05df0f
Kyle <kyleferguson91@gmail.com>
2022-08-25 13:39:29 -0400

    chaned decimal logic fixed numpadenter selecting buttons

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 
 const screen = document.querySelector(".screentexts")
+const lcd = document.querySelector(".screen")
+const buttons = document.querySelectorAll("button")
+const calcbody = document.querySelector(".calculatorbody")
 
 
-function add (a,b) {
-    console.log(+a + +b);
-    screen.textContent =  parseFloat((+a + +b).toFixed(7))
+let input = []
+let operand = "";
+let value1 = "";
+const decimal = document.querySelector(".decimal")
 
+
+
+
+function add(a = 0, b = 0) {
+    console.log(a + b);
   
+ 
+screen.textContent = parseFloat((+a + +b).toFixed(7))
 
-    return (+a + +b)
+
+
+    return (parseFloat((+a + +b).toFixed(7)))
 }
 
-function subtract (a,b) {
-   
+function subtract(a, b) {
+
     console.log(+a - +b)
-    screen.textContent =  parseFloat((+a - +b).toFixed(7))
-    return +a - +b
+    screen.textContent = parseFloat((+a - +b).toFixed(7))
+    return parseFloat((+a - +b).toFixed(7))
 }
 
-function multiply (a,b) {
-    screen.textContent = +a * +b
-    return a * b
+function multiply(a, b) {
+    screen.textContent = parseFloat((+a * +b).toFixed(7))
+
+    return parseFloat((+a * +b).toFixed(7))
 }
-  
 
 
-function divide (a,b) {
-    if (a == 0 || b == 0) {screen.textContent = "Can't dun divihud by zeruh"}
-else {    screen.textContent = +a / +b
-return +a / +b}
+
+function divide(a, b) {
+
+    if (a == 0 || b == 0) { screen.textContent = "Can't dun divihud by zeruh" }
+    else {
+        screen.textContent = parseFloat((+a / +b).toFixed(2))
+        return +a / +b
+    }
 }
 
 function switcher(a) {
@@ -40,167 +57,100 @@ function switcher(a) {
 
 
 
-function operate(operator,a,b) {
+function operate(operator, a, b) {
 
-if (operator == "+") {
-    add(a,b)}
-else if (operator == "-") {subtract(a,b)}
-else if (operator == "*") {multiply(a,b)}
-else if (operator == "/") {divide(a,b)}
-else if (operator == "%") {percent(a,b)}
-else if (operator == "+/-") {switcher(a)}
+    if (operator == "+") {
+        add(a, b)
+    }
+    else if (operator == "-") { subtract(a, b) }
+    else if (operator == "*") { multiply(a, b) }
+    else if (operator == "/") { divide(a, b) }
+    else if (operator == "%") { percent(a, b) }
+    else if (operator == "+/-") { switcher(a) }
 
 
 }
 
-window.addEventListener('', e => {
-    console.log(e.key, "key") 
-    {
-        
-
-      
-
-        let displayValue = e.key
-
-        if (e.key == "AC") {
-            document.getElementById("dec").setAttribute("disabled", "false")
-            screen.textContent = ""
-            input = []
-            console.log(input)
-            operand = "";
-            value1 = "";
-            current = "";
-           
-        }
-
-     
-            else if (e.key == ".") {
-                screen.textContent += e.key
-                input.push(e.key)
-            console.log(input)
-            document.getElementById("dec").setAttribute("disabled", "true")
-            //document.querySelector(".newcontainer").removeChild(decimal)
-            }
 
 
-
-
-                else if (
-                    e.key == "+" ||
-                         e.key == "-" ||
-                         e.key == "*" ||
-                         e.key == "/" ||
-                         e.key == "%"  ) {
-                            document.getElementById("dec").removeAttribute("disabled")
-                            value1 = screen.textContent;
-                            operand = e.key;
-                
-                            input.push(e.key)
-                            screen.textContent = "" 
-                          
-                            input = []  
-                      
-
-
-
-                }
-          
-
-
-        else if (e.key == "Enter") {
-
-            current = input.join("")
-            console.log(operand,value1,current, "operand, value1, current")
-            value1 = +value1
-            current = +current
-            operate(operand, value1, current ) 
-            document.getElementById("dec").setAttribute("disabled", "false")
-      
-            
-        }
-
-        else {
-            screen.textContent += e.key
-            input.push(e.target.textContent)
-        console.log(input)}
-        
-
-
-
-
+window.addEventListener("keydown", e => {
+    if (input.some(elem => {elem == "."})) {
+        document.getElementById("dec").setAttribute("disabled", "true")
     }
 
+   console.log(e.key, "leytracker")
 
-})
-
-window.addEventListener("keydown", e=> {
-
-console.log(e.key)
-
-if (!isNaN(parseInt(e.key))) {
-    console.log(e.key)
-    if (screen.textContent == "0") {screen.textContent = ""}
-    screen.textContent += e.key
-    input.push(e.key)
-    console.log(input)
-
-}
-
-else if (e.key == "Enter") {
-    console.log("enter here")
-    current = input.join("")
-    console.log(operand,value1,current, "operand, value1, current")
-    value1 = +value1
-    current = +current
-    operate(operand, value1, current ) 
-    document.getElementById("dec").setAttribute("disabled", "false")
- 
-
-}
-
-else if (e.key == "Backspace") {
-    input = screen.textContent.split("")
-    if (input[input.length-1] == ".") {
-        document.getElementById("dec").removeAttribute("disabled")
-    }
-   
-     input.pop()
-     screen.textContent = input.join("")
+    if (!isNaN(parseInt(e.key))) {
+  
+        if (screen.textContent == "0") { screen.textContent = "" }
+        screen.textContent += e.key
+        input.push(e.key)
         console.log(input)
 
-}
+    }
 
-else if (e.key == "+" ||
-        e.key == "-" ||
-        e.key == "/" ||
-        e.key == "*")
-        {
-            console.log("operator")
+    else if (e.key == "=" || e.key == "Enter") {
+        
+        e.cancelBubble = true;
+        
+        e.preventDefault()
+        current = input.join("")
+        value1 = +value1
+        current = +current
+            document.getElementById("dec").setAttribute("disabled", "false")
+            operate(operand, value1, current)
+    }
+
+    else if (e.key == "Backspace") {
+        input = screen.textContent.split("")
+        if (input[input.length - 1] == ".") {
             document.getElementById("dec").removeAttribute("disabled")
-            value1 = screen.textContent;
-            operand = e.key;
-
-            input.push(e.key)
-            screen.textContent = "" 
-          
-            input = []  
-      
-
-
-
-
         }
 
+        input.pop()
+        screen.textContent = input.join("")
+        console.log(input)
 
-else if (e.key == ".") {
-console.log("decimal here")
-screen.textContent += e.key
-input.push(e.key)
-console.log(input)
-document.getElementById("dec").setAttribute("disabled", "true")
+    }
+
+    else if (e.key == "+" ||
+        e.key == "-" ||
+        e.key == "/" ||
+        e.key == "*") {
+  
+        if (screen.textContent == "0") { screen.textContent = "" }
+        document.getElementById("dec").removeAttribute("disabled")
+        value1 = +screen.textContent
+        operand = e.key + "";
+
+        input.push(e.key)
+        screen.textContent = ""
+
+        input = []
 
 
-}
+
+
+
+    }
+
+
+    else if (e.key == ".") {
+        console.log("decimal here")
+     
+        if (input.indexOf(".") >= 0) {
+            screen.textContent == screen.textContent
+        }
+        else {  
+             screen.textContent += e.key
+            input.push(e.key)
+            document.getElementById("dec").setAttribute("disabled", "true")}
+      
+ 
+   
+
+
+    }
 
 
 })
@@ -210,115 +160,116 @@ document.getElementById("dec").setAttribute("disabled", "true")
 
 
 
-const buttons = document.querySelectorAll("button")
 
+//const innerline = document.querySelector(".eqtext")
 
-let input = []
-let operand = "";
-let value1 = "";
-const decimal = document.querySelector(".decimal")
-
-buttons.forEach((elem,ind,arr) => {
+buttons.forEach((elem, ind, arr) => {
 
 
 
     elem.addEventListener("click", (e) => {
-        
 
-      
+        if (input.some(elem => {elem == "."})) {
+            document.getElementById("dec").setAttribute("disabled", "true")
+        }
+
 
         let displayValue = e.target.textContent
 
         if (e.target.textContent == "AC") {
+            e.target.blur
             document.getElementById("dec").removeAttribute("disabled", "false");
+            input = [0]
             screen.textContent = "0"
-            input = []
-            console.log(input)
-         
-            operand = "";
-            value1 = "";
-            current = "";
+            e.stopPropagation()          
            
-           
+    
+
+
         }
 
-            else if (e.target.textContent == "+/-") {
-                operate(e.target.textContent, screen.textContent)
-            }
+        else if (e.target.textContent == "+/-") {
+            operate(e.target.textContent, screen.textContent)
+        }
 
-            else if (e.target.textContent == ".") {
-                screen.textContent += e.target.textContent
-                input.push(e.target.textContent)
+        else if (e.target.textContent == ".") {
+            screen.textContent += e.target.textContent
+            input.push(e.target.textContent)
             console.log(input)
             document.getElementById("dec").setAttribute("disabled", "true")
             //document.querySelector(".newcontainer").removeChild(decimal)
-            }
+        }
 
 
         else if (e.target.textContent == "<") {
             input = screen.textContent.split("")
-            if (input[input.length-1] == ".") {
+            if (input[input.length - 1] == ".") {
                 document.getElementById("dec").removeAttribute("disabled")
             }
-           
-             input.pop()
-             screen.textContent = input.join("")
-                console.log(input)
 
-            }
+            input.pop()
+            screen.textContent = input.join("")
+            console.log(input)
 
-
-                else if (
-                    e.target.textContent == "+" ||
-                         e.target.textContent == "-" ||
-                         e.target.textContent == "*" ||
-                         e.target.textContent == "/" ||
-                         e.target.textContent == "%"  ) {
-                            document.getElementById("dec").removeAttribute("disabled")
-                            value1 = screen.textContent;
-                            operand = e.target.textContent;
-                
-                            input.push(e.target.textContent)
-                            screen.textContent = "" 
-                          
-                            input = []  
-                      
+        }
 
 
+        else if (
+            e.target.textContent == "+" ||
+            e.target.textContent == "-" ||
+            e.target.textContent == "*" ||
+            e.target.textContent == "/" ||
+            e.target.textContent == "%") {
+            document.getElementById("dec").removeAttribute("disabled")
 
-                }
-          
+            //innerline.textContent = input.join("")+ " " +e.target.textContent + " "
+            value1 = screen.textContent;
+            operand = e.target.textContent;
+            input.push(e.target.textContent)
+            screen.textContent = ""
+
+            input = []
+
+
+
+
+
+        }
+
 
 
         else if (e.target.textContent == "=") {
 
             current = input.join("")
-            console.log(operand,value1,current, "operand, value1, current")
+            console.log(operand, value1, current, "operand, value1, current")
             value1 = +value1
             current = +current
-            
-            operate(operand, value1, current ) 
-  
+
+
+            operate(operand, value1, current)
+
+
             console.log(input)
             document.getElementById("dec").setAttribute("disabled", "false")
-         
-           
-           
-            
+
+
+
+
         }
 
         else {
-            if (screen.textContent == "0") {screen.textContent = ""}
+            if (screen.textContent == "0") { screen.textContent = "" }
             screen.textContent += e.target.textContent
             input.push(e.target.textContent)
-        console.log(input)}
-        
+            console.log(input)
+        }
+
 
 
 
 
     })
-        
+
 
 
 
