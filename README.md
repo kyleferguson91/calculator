2cd89be60b838b7b1850ebc4803828e10764901e
Kyle <kyleferguson91@gmail.com>
2022-08-25 10:21:35 -0400

    calculator functions working printing to screen

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # calculator
+
+
+        <div class="buttons">
+            <div class="numbers" id="foo">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <br><br><br><br><br><br><br><br><br><br><br>
+                <button id="zero">0</button>
+            </div>
+            <div class="operators">
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <br><br><br><br><br><br><br><br>
+
+                <button class="last">clear</button>
+                <button class="last"><</button>
+                <button id="equals">=</button>
+
+            </div>
+        </div>
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 const screen = document.querySelector(".screentexts")
+
+
 function add (a,b) {
     console.log(+a + +b)
    // screen.textContent = (+a + +b)
@@ -7,22 +9,30 @@ function add (a,b) {
 }
 
 function subtract (a,b) {
-    console.log(a-b)
-  //  screen.textContent = +a - +b
+    console.log(+a - +b)
+   screen.textContent = +a - +b
     return +a - +b
 }
 
 function multiply (a,b) {
-   // screen.textContent = +a * +b
-    return +a * +b
+    screen.textContent = +a * +b
+    return a * b
 }
   
 
 
 function divide (a,b) {
-   // screen.textContent = +a / +b
+   screen.textContent = +a / +b
     return +a / +b
 }
+
+
+function percent (a,b) {
+    screen.textContent = +a / +b
+     return +a % +b
+ }
+
+
 
 function operate(operator,a,b) {
 
@@ -31,12 +41,15 @@ if (operator == "+") {
 else if (operator == "-") {subtract(a,b)}
 else if (operator == "*") {multiply(a,b)}
 else if (operator == "/") {divide(a,b)}
+else if (operator == "%") {percent(a,b)}
 
 
 
 }
 
-
+window.addEventListener('keydown', e => {
+    console.log(e.key)
+})
 
 
 
@@ -48,7 +61,8 @@ const buttons = document.querySelectorAll("button")
 
 
 let input = []
-
+let operand = "";
+let value1 = "";
 
 buttons.forEach((elem,ind,arr) => {
 
@@ -61,27 +75,34 @@ buttons.forEach((elem,ind,arr) => {
 
         let displayValue = e.target.textContent
 
-        if (e.target.textContent == "clear") {
+        if (e.target.textContent == "AC") {
             screen.textContent = ""
             input = []
             console.log(input)}
 
 
         else if (e.target.textContent == "<") {
-           console.log( )
-            
+       
              input.pop()
              screen.textContent = input.join("")
-                console.log(input)}
+                console.log(input)
 
-                else if (e.target.textContent == "+" ||
+            }
+
+
+                else if (
+                    e.target.textContent == "+" ||
                          e.target.textContent == "-" ||
                          e.target.textContent == "*" ||
                          e.target.textContent == "/") {
-                           
+                            
+                            value1 = screen.textContent;
+                            operand = e.target.textContent;
+                           console.log(value1, operand)
                             input.push(e.target.textContent)
                             screen.textContent = "" 
-                            console.log(input)   
+                          
+                            input = []  
 
 
 
@@ -90,14 +111,19 @@ buttons.forEach((elem,ind,arr) => {
 
 
         else if (e.target.textContent == "=") {
-            screen.textContent += e.target.textContent
-           operate(input[1], input[0], input[2]) 
+
+            current = input.join("")
+            console.log(operand,value1,current, "operand, value1, current")
+            value1 = +value1
+            current = +current
+            operate(operand, value1, current ) 
+           
       
             
         }
 
         else {
-            
+            screen.textContent += e.target.textContent
             input.push(e.target.textContent)
         console.log(input)}
         
